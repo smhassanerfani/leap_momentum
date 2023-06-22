3f258f7dd97ed84e9001365a238aedb3c5fbd370
smhassanerfani <serfani@email.sc.edu>
2023-06-22 13:59:45 +0000

    Added csv_writer function

--- utils.py
+++ utils.py
@@ -2,6 +2,24 @@ import torch
 from torchvision.utils import save_image
 import matplotlib.pyplot as plt
 import xarray as xr
+import csv
+
+def csv_writer(logger, save_path):
+
+    with open(f"{save_path}/loss_log.csv", 'a+', newline='') as filehandler:
+
+        w = csv.DictWriter(filehandler, logger.keys())
+        w.writerow(logger)
+        
+#     fieldnames = logger[0].keys()
+
+#     with open(f"{save_path}/loss_log.csv", 'a+', newline='') as filehandler:
+#         fh_writer = csv.DictWriter(filehandler, fieldnames=fieldnames)
+
+#         fh_writer.writeheader()
+#         for item in logger:
+#             fh_writer.writerow(item)
+
 
 class AdjustLearningRate:
     num_of_iterations = 0
@@ -40,15 +58,15 @@ def load_checkpoint(checkpoint_file, model, optimizer, lr):
         param_group["lr"] = lr
 
 
-def save_examples(x, y, y_fake, dataset, counter, saving_path):
+def save_examples(x, y, y_fake, transform_params, counter, saving_path):
     
     fig, axes = plt.subplots(nrows= x.shape[0], ncols=3, figsize=(12, 13), constrained_layout=True)
     
     for idx in range(x.shape[0]):
     
-        image = x[idx].detach().cpu().numpy() * dataset.inputs_mean_std[1] + dataset.inputs_mean_std[0]
-        target = y[idx].detach().cpu().numpy() * dataset.targets_mean_std[1] + dataset.targets_mean_std[0]
-        gen_target = y_fake[idx].detach().cpu().numpy() * dataset.targets_mean_std[1] + dataset.targets_mean_std[0]
+        image = x[idx].detach().cpu().numpy() * transform_params['inputs_std'] + transform_params['inputs_mean']
+        target = y[idx].detach().cpu().numpy() * transform_params['targets_std'] + transform_params['targets_mean']
+        gen_target = y_fake[idx].detach().cpu().numpy() * transform_params['targets_std'] + transform_params['targets_mean']
 
         xr.DataArray(image.squeeze(), dims=['x', 'y']).plot(x="x", y="y", robust=True, yincrease=False, ax=axes[idx, 0])
         xr.DataArray(target.squeeze(), dims=['x', 'y']).plot(x="x", y="y", robust=True, yincrease=False, ax=axes[idx, 1])
@@ -56,4 +74,6 @@ def save_examples(x, y, y_fake, dataset, counter, saving_path):
 
     plt.savefig(f'{saving_path}/{counter}.png', format='png', bbox_inches='tight', pad_inches=0.1)
     plt.close()
+    
+    del x, y, y_fake
 
