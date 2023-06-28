d461beb0d953da8a0bd86fff5803153562079b37
smhassanerfani <serfani@email.sc.edu>
2023-06-28 16:02:45 +0000

    Modified for new model

--- train.py
+++ train.py
@@ -1,20 +1,37 @@
+import os
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 import torchvision
 from joint_transforms import Transform
-from torch.utils.tensorboard import SummaryWriter
-from models.Dhruv import Generator, initialize_weights
+from models.Dhruv import Generator, DnCNN, initialize_weights
 from dataset import Agulhas
-from utils.utils import AdjustLearningRate
+from utils.utils import AdjustLearningRate, save_examples
 
+
+def val_loop(dataloader, transform_params, model, saving_path):
+
+    model.eval()
+    with torch.no_grad():
+        for counter, (x, y) in enumerate(dataloader, 1):
+
+            # GPU deployment
+            x = x.cuda()
+            y = y.cuda()
+
+            # Compute prediction and loss
+            y_fake = model(x)
+
+            save_examples(x, y, y_fake, transform_params, counter, saving_path)
+            if counter == 5:
+                break
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(torch.cuda.get_device_name(device))
 
-LEARNING_RATE = 2.0E-5
-BATCH_SIZE = 4
+LEARNING_RATE = 2.0E-4
+BATCH_SIZE = 64
 IMAGE_SIZE = 256
 CHANNELS_IMG = 1
 NUM_EPOCHS = 30
@@ -26,20 +43,25 @@ joint_transforms = Transform()
 dataset = Agulhas(split='train', joint_transform=joint_transforms)
 dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
 
-model = Generator(CHANNELS_IMG, FEATURES).to(device)
+val_dataset = Agulhas(split='val', joint_transform=joint_transforms)
+val_dataloader = DataLoader(val_dataset, batch_size=4, shuffle=True)
+
+transform_params = dict()
+transform_params['inputs_mean'] = dataset.inps_mean_std[0]
+transform_params['inputs_std'] = dataset.inps_mean_std[1]
+transform_params['targets_mean'] = dataset.tars_mean_std[0]
+transform_params['targets_std'] = dataset.tars_mean_std[1]
+    
+
+model = DnCNN(CHANNELS_IMG, FEATURES).to(device)
 initialize_weights(model)
 
 optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
-criterion = nn.MSELoss()
+criterion1 = nn.MSELoss()
 
 
-max_iter = NUM_EPOCHS * len(dataloader.dataset)
-scheduler = AdjustLearningRate(optimizer, LEARNING_RATE, max_iter, 0.9)
-
-writer_real = SummaryWriter('logs/LEAP/real')
-writer_fake = SummaryWriter('logs/LEAP/fake')
-writer_loss = SummaryWriter('logs/LEAP/loss')
-step = 0
+# max_iter = NUM_EPOCHS * len(dataloader.dataset)
+# scheduler = AdjustLearningRate(optimizer, LEARNING_RATE, max_iter, 0.9)
 
 model.train()
 
@@ -51,31 +73,26 @@ for epoch in range(NUM_EPOCHS):
 
         git = model(ssh)
         
-        loss = criterion(git, it)
+        loss1 = criterion1(git, it)
+        loss2 = criterion2(git, it)
+        loss = loss1 + loss2
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        scheduler.num_of_iterations += ssh.size(0)
-        lr = scheduler(scheduler.num_of_iterations)
+        # scheduler.num_of_iterations += ssh.size(0)
+        # lr = scheduler(scheduler.num_of_iterations)
 
         if batch_idx % 100 == 0:
-            print(f"Epoch: [{epoch}/{NUM_EPOCHS}]\tBatch: {batch_idx:>2}/{len(dataloader)} "
-                  f"Loss: {loss.item():.4f}, LR:{lr:#.4E}")
-
-            with torch.no_grad():
-
-                writer_loss.add_scalar('Model Loss', loss.item(), global_step=step)
-
-                git = model(ssh)
-                
-                it = it * dataset.tars_mean_std[0] + dataset.tars_mean_std[0]
-                git = git * dataset.tars_mean_std[0] + dataset.tars_mean_std[0]
-                
-                img_grid_fake = torchvision.utils.make_grid(it, normalize=False)
-                img_grid_real = torchvision.utils.make_grid(git, normalize=False)
-
-                writer_fake.add_image("Fake Images", img_grid_fake, global_step=step)
-                writer_real.add_image("Real Images", img_grid_real, global_step=step)
-                step += 1
+            print(f"Epoch: [{epoch}/{NUM_EPOCHS}] Batch: {batch_idx:>2}/{len(dataloader)} "
+                  f"Loss: {loss.item():.4f},")# LR:{lr:#.4E}")
+    
+    current_dir = os.path.join('outputs', 'Dhruv', 'trial4', f'epoch-{epoch:02d}')
+    
+    try:
+        os.makedirs(current_dir)
+    except FileExistsError:
+        pass
+    
+    val_loop(val_dataloader, transform_params, model, current_dir)
