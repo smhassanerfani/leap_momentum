efa48c107a34103aad756550c0d12c58d2c06a83
smhassanerfani <serfani@email.sc.edu>
2023-07-10 20:18:19 +0000

    Debugged the np.save loss func

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -6,8 +7,8 @@ from torch.utils.data import DataLoader
 import torchvision
 from joint_transforms import Transform2
 from models.Dhruv import Generator, DnCNN, initialize_weights
-from dataset import Agulhas2
-from utils.utils import AdjustLearningRate, save_examples2
+from dataset import Agulhas2, Agulhas3
+from utils.utils import AdjustLearningRate, save_checkpoint, save_examples2, save_examples3
 
 
 def val_loop(dataloader, transform_params, model, saving_path):
@@ -28,7 +29,7 @@ def val_loop(dataloader, transform_params, model, saving_path):
             y_fake = torch.cat([y_it_fake, y_bm_fake], dim=1)
             y = torch.cat([y_it, y_bm], dim=1)
 
-            save_examples2(x, y, y_fake, transform_params, counter, saving_path)
+            save_examples3(x, y, y_fake, transform_params, counter, saving_path)
             if counter == 5:
                 break
 
@@ -39,38 +40,31 @@ LEARNING_RATE = 2.0E-4
 BATCH_SIZE = 2
 IMAGE_SIZE = 256
 CHANNELS_IMG = 1
-NUM_EPOCHS = 100
+NUM_EPOCHS = 101
 FEATURES = 64
 
 
 joint_transforms = Transform2()
 
-dataset = Agulhas2(split='train', joint_transform=joint_transforms)
+dataset = Agulhas3(split='train', joint_transform=joint_transforms)
 dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
 
-val_dataset = Agulhas2(split='val', joint_transform=joint_transforms)
+val_dataset = Agulhas3(split='val', joint_transform=joint_transforms)
 val_dataloader = DataLoader(val_dataset, batch_size=2, shuffle=True)
 
-transform_params = dict()
-transform_params['inputs_mean'] = dataset.inps_mean_std[0]
-transform_params['inputs_std'] = dataset.inps_mean_std[1]
-transform_params['targets_mean'] = dataset.tars_mean_std[0]
-transform_params['targets_std'] = dataset.tars_mean_std[1]
-transform_params['targets_bm_mean'] = dataset.tars_bm_mean_std[0]
-transform_params['targets_bm_std'] = dataset.tars_bm_mean_std[1]
+transform_params = dataset.transform_dict
     
-
 model = DnCNN(CHANNELS_IMG, FEATURES).to(device)
 initialize_weights(model)
 
-optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
-criterion1 = nn.MSELoss()
-criterion2 = nn.L1Loss()
+optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+criterion = nn.L1Loss()
 
 # max_iter = NUM_EPOCHS * len(dataloader.dataset)
 # scheduler = AdjustLearningRate(optimizer, LEARNING_RATE, max_iter, 0.9)
 
 model.train()
+loss_list = list()
 
 for epoch in range(NUM_EPOCHS):
     for batch_idx, (ssh, it, bm) in enumerate(dataloader):
@@ -79,16 +73,12 @@ for epoch in range(NUM_EPOCHS):
         it = it.to(device)
         bm = bm.to(device)
 
-        out = model(ssh)
-        it_out = ssh - out
+        bm_out = model(ssh)
+        it_out = ssh - bm_out
         
-        # loss1 = criterion1(out, bm)
-        loss2 = criterion2(out, bm)
-
-        loss3 = criterion1(it_out, it)
-        # loss4 = criterion2(it_out, it)
-
-        loss = (loss2 + loss3)
+        loss1 = criterion(bm_out, bm)
+        loss2 = criterion(it_out, it)
+        loss = (loss1 + loss2) / 2
 
         optimizer.zero_grad()
         loss.backward()
@@ -98,14 +88,20 @@ for epoch in range(NUM_EPOCHS):
         # lr = scheduler(scheduler.num_of_iterations)
 
         if batch_idx % 100 == 0:
-            print(f"Epoch: [{epoch}/{NUM_EPOCHS}] Batch: {batch_idx:>2}/{len(dataloader)} "
-                  f"Loss: {loss.item():.4f},")# LR:{lr:#.4E}")
+            print(f"Epoch: [{epoch}/{NUM_EPOCHS}] Batch: {batch_idx:>2}/{len(dataloader)} Loss: {loss.item():.4f},")
+            loss_list.append([loss1.item(), loss2.item()])
     
-    current_dir = os.path.join('outputs', 'Dhruv', 'trial11', f'epoch-{epoch:02d}')
+    current_dir = os.path.join('outputs', 'DnCNN', 'T05', f'epoch-{epoch:003d}')
     
     try:
         os.makedirs(current_dir)
     except FileExistsError:
         pass
     
+    if epoch % 20 == 0:
+        save_checkpoint(model, optimizer,  os.path.join(current_dir, "model.pth.tar"))
+    
     val_loop(val_dataloader, transform_params, model, current_dir)
+
+with open(os.path.join(current_dir, "loss.npy"), mode = 'wb') as f:
+    np.save(f, np.array(loss_list))
