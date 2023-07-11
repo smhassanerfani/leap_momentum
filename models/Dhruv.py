b6372ec90f38eb5265ac25a7d3096245acd83d28
smhassanerfani <serfani@email.sc.edu>
2023-07-11 14:54:02 +0000

    Modified the head for two outputs

--- models/Dhruv.py
+++ models/Dhruv.py
@@ -1,56 +1,6 @@
 import torch
 import torch.nn as nn
 
-class Generator(nn.Module):
-    def __init__(self, img_channels=1, num_features=4):
-        super().__init__()
-        self.lyr1 = nn.Conv2d(img_channels, num_features, kernel_size=3, stride=1, bias=False, padding=1, padding_mode='reflect')
-        self.bn1  = nn.InstanceNorm2d(num_features, affine=True)
-        self.act1 = nn.ReLU()
-        
-        self.lyr2 = nn.Conv2d(num_features, num_features*2, kernel_size=3, stride=1, bias=False, padding=1, padding_mode='reflect')
-        self.bn2  = nn.InstanceNorm2d(num_features*2, affine=True)
-        self.act2 = nn.ReLU()
-        
-        self.lyr3 = nn.Conv2d(num_features*2, num_features*4, kernel_size=3, stride=1, bias=False, padding=1, padding_mode='reflect')
-        self.bn3  = nn.InstanceNorm2d(num_features*4, affine=True)
-        self.act3 = nn.ReLU()
-        
-        
-        self.lyr4 = nn.Conv2d(num_features*4, num_features*8, kernel_size=3, stride=1, bias=False, padding=1, padding_mode='reflect')
-        self.bn4  = nn.InstanceNorm2d(num_features*8, affine=True)
-        self.act4 = nn.ReLU()
-        
-        
-        self.lyr5 = nn.Conv2d(num_features*8, num_features, kernel_size=3, stride=1, bias=False, padding=1, padding_mode='reflect')
-        self.bn4  = nn.InstanceNorm2d(num_features, affine=True)
-        self.act5 = nn.ReLU()
-        
-        self.lyr6 = nn.Conv2d(num_features, 1, kernel_size=3, stride=1, padding=1, padding_mode='reflect')
-          
-    
-    def forward(self, x):
-        
-        output = self.lyr1(x)
-        output = self.act1(output)
-
-        output = self.lyr2(output)
-        output = self.act2(output)
-
-        output = self.lyr3(output)
-        output = self.act3(output)
-
-        output = self.lyr4(output)
-        output = self.act4(output)
-
-        output = self.lyr5(output)
-        output = self.act5(output)
-
-        output = self.lyr6(output)
-            
-        return torch.tanh(output)
-
-    
 class DnCNN(nn.Module):
     def __init__(self, in_channels=1, num_features=64):
         super().__init__()
@@ -60,13 +10,14 @@ class DnCNN(nn.Module):
         )
     
         self.cnn = nn.Sequential(
+            self._block(num_features, num_features, 3, 1, 1),
             self._block(num_features, num_features*2, 3, 1, 1),
             self._block(num_features*2, num_features*4, 3, 1, 1),
-            self._block(num_features*4, num_features*4, 3, 1, 1),
             self._block(num_features*4, num_features*4, 3, 1, 1),
             self._block(num_features*4, num_features*2, 3, 1, 1),
             self._block(num_features*2, num_features, 3, 1, 1)
         )
+        
         self.head = nn.Conv2d(num_features, in_channels, kernel_size=3, padding=1, padding_mode='reflect')
     
     def _block(self, in_channels, out_channels, kernel_size, stride, padding):
@@ -77,24 +28,16 @@ class DnCNN(nn.Module):
         )
     
     def forward(self, x):
+        
         out = self.initialization(x)
         out = self.cnn(out)
-        out = self.head(out) + x
-        return torch.tanh(out)
+        out = self.head(out)
+        
+        BM = out + x
+        IT = -1 * out
+        
+        return torch.tanh(BM), torch.tanh(IT) 
 
-
-class CNN(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.cnn = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=8, kernel_size=7, padding=(3,3)), nn.PReLU(),
-            nn.Conv2d(in_channels=8, out_channels=8, kernel_size=7, padding=(3,3)), nn.PReLU(),
-            nn.Conv2d(in_channels=8, out_channels=8, kernel_size=7, padding=(3,3)), nn.PReLU(),
-            nn.Conv2d(in_channels=8, out_channels=8, kernel_size=7, padding=(3,3)), nn.PReLU(),
-            nn.Conv2d(in_channels=8, out_channels=8, kernel_size=7, padding=(3,3)), nn.PReLU()
-                     )
-    def forward(self, x):
-        return self.cnn(x)
 
 def initialize_weights(model):
     for m in model.modules():
