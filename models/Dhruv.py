82bc600436bc5911f1819fc134caa63e26670976
smhassanerfani <serfani@email.sc.edu>
2023-07-14 16:30:31 +0000

    Added simple noise canceling model

--- models/Dhruv.py
+++ models/Dhruv.py
@@ -1,6 +1,75 @@
 import torch
 import torch.nn as nn
 
+class DnCNN_v0(nn.Module):
+    
+    def __init__(self, in_channels=1, num_features=16):
+        super().__init__()
+        self.initialization = nn.Sequential(
+            nn.Conv2d(in_channels, num_features, kernel_size=3, padding=1, padding_mode='reflect'),
+            nn.PReLU()
+        )
+    
+        self.cnn = nn.Sequential(
+            self._block(num_features, num_features, 3, 1, 1),
+            self._block(num_features, num_features, 3, 1, 1),
+            self._block(num_features, num_features, 3, 1, 1),
+            self._block(num_features, num_features, 3, 1, 1),
+            self._block(num_features, num_features, 3, 1, 1),
+            self._block(num_features, num_features, 3, 1, 1)
+        )
+        
+        self.head = nn.Conv2d(num_features, in_channels, kernel_size=3, padding=1, padding_mode='reflect')
+    
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, padding_mode='reflect', bias=False),
+            nn.InstanceNorm2d(out_channels, affine=True),
+            nn.PReLU()
+        )
+    
+    def forward(self, x):
+        
+        out = self.initialization(x)
+        out = self.cnn(out)
+        out = self.head(out) + x
+        
+        return torch.tanh(out)
+
+
+class DnCNN_v1(nn.Module):
+    def __init__(self, in_channels=1, num_features=64):
+        super().__init__()
+        self.initialization = nn.Sequential(
+            nn.Conv2d(in_channels, num_features, kernel_size=3, padding=1, padding_mode='reflect'),
+            nn.PReLU()
+        )
+    
+        self.cnn = nn.Sequential(
+            self._block(num_features, num_features*2, 3, 1, 1),
+            self._block(num_features*2, num_features*4, 3, 1, 1),
+            self._block(num_features*4, num_features*4, 3, 1, 1),
+            self._block(num_features*4, num_features*4, 3, 1, 1),
+            self._block(num_features*4, num_features*2, 3, 1, 1),
+            self._block(num_features*2, num_features, 3, 1, 1)
+        )
+        
+        self.head = nn.Conv2d(num_features, in_channels, kernel_size=3, padding=1, padding_mode='reflect')
+    
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, padding_mode='reflect', bias=False),
+            nn.InstanceNorm2d(out_channels, affine=True),
+            nn.PReLU()
+        )
+    
+    def forward(self, x):
+        
+        out = self.initialization(x)
+        out = self.cnn(out)
+        out = self.head(out) + x
+        return torch.tanh(out)
+
 class DnCNN(nn.Module):
     def __init__(self, in_channels=1, num_features=64):
         super().__init__()
--- train.py
+++ train.py
@@ -2,11 +2,13 @@ import os
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader
+import torch.backends.cudnn as cudnn
 import torchvision
 from joint_transforms import Transform2
-from models.Dhruv import DnCNN, initialize_weights
+from models.Dhruv import DnCNN, DnCNN_v0, initialize_weights
 from dataset import Agulhas2, Agulhas3
 from utils.utils import AdjustLearningRate, save_checkpoint, save_examples2, save_examples3
 
@@ -23,7 +25,8 @@ def val_loop(dataloader, transform_params, model, saving_path):
             bm = bm.cuda()
 
             # Compute prediction and loss
-            bm_, it_ = model(ssh)
+            bm_ = model(ssh)
+            it_ = ssh - bm_
             
             y = torch.cat([it, bm], dim=1)
             y_ = torch.cat([it_, bm_], dim=1)
@@ -33,6 +36,15 @@ def val_loop(dataloader, transform_params, model, saving_path):
             if counter == 5:
                 break
 
+
+def psnr(original, reconstructed):
+    
+    # R is the maximum fluctuation in the input image data type. [-1, +1] -> 2.0
+    mse = F.mse_loss(original, reconstructed)
+    psnr = 20 * torch.log10(2.0 / torch.sqrt(mse))
+
+    return psnr
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(torch.cuda.get_device_name(device))
 
@@ -41,8 +53,7 @@ BATCH_SIZE = 2
 IMAGE_SIZE = 256
 CHANNELS_IMG = 1
 NUM_EPOCHS = 101
-FEATURES = 64
-
+FEATURES = 16
 
 joint_transforms = Transform2()
 
@@ -54,30 +65,38 @@ val_dataloader = DataLoader(val_dataset, batch_size=2, shuffle=True)
 
 transform_params = dataset.transform_dict
     
-model = DnCNN(CHANNELS_IMG, FEATURES).to(device)
+model = DnCNN_v0(CHANNELS_IMG, FEATURES).to(device)
 initialize_weights(model)
 
 optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
-criterion = nn.MSELoss()
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.5)
+
+criterion = nn.L1Loss()
+# criterion = nn.MSELoss()
 
 # max_iter = NUM_EPOCHS * len(dataloader.dataset)
 # scheduler = AdjustLearningRate(optimizer, LEARNING_RATE, max_iter, 0.9)
 
+cudnn.enabled = True
+cudnn.benchmark = True
 model.train()
-loss_list = list()
+
+logger = {'loss': list(), 'psnr': list()}
 
 for epoch in range(NUM_EPOCHS):
+    print('Epoch:', epoch,'LR:', scheduler.get_last_lr())
+    
+    batch_loss = 0.0
+    batch_psnr = 0.0
     for batch_idx, (ssh, it, bm) in enumerate(dataloader):
 
         ssh = ssh.to(device)
-        it = it.to(device)
+        # it = it.to(device)
         bm = bm.to(device)
 
-        bm_, it_ = model(ssh)
+        bm_ = model(ssh)
         
-        bm_loss = criterion(bm_, bm)
-        it_loss = criterion(it_, it)
-        loss = (bm_loss + it_loss) / 2
+        loss = criterion(bm_, bm)
 
         optimizer.zero_grad()
         loss.backward()
@@ -85,22 +104,32 @@ for epoch in range(NUM_EPOCHS):
 
         # scheduler.num_of_iterations += ssh.size(0)
         # lr = scheduler(scheduler.num_of_iterations)
+        
+        loss = loss.detach()
+        batch_loss += loss.item()
+        batch_psnr += psnr(bm.detach(), bm_.detach()).cpu().numpy()
 
         if batch_idx % 100 == 0:
-            print(f"Epoch: [{epoch}/{NUM_EPOCHS}] Batch: {batch_idx:>2}/{len(dataloader)} Loss: {loss.item():.4f},")
-            loss_list.append([bm_loss.item(), it_loss.item()])
+            print(f"Epoch: [{epoch+1}/{NUM_EPOCHS}] Batch: {batch_idx:>2}/{len(dataloader)} Loss: {loss.item():.4f},")
     
-    current_dir = os.path.join('outputs', 'DnCNN', 'T06', f'epoch-{epoch:003d}')
+    scheduler.step()
+    
+    logger['loss'].append(batch_loss / len(dataloader))
+    logger['psnr'].append(batch_psnr / len(dataloader))
+
+    current_dir = os.path.join('outputs', 'DnCNN', 'T08', f'epoch-{epoch:003d}')
     
     try:
         os.makedirs(current_dir)
     except FileExistsError:
         pass
     
-    if epoch % 20 == 0:
+    if epoch % 10 == 0:
         save_checkpoint(model, optimizer,  os.path.join(current_dir, "model.pth.tar"))
     
     val_loop(val_dataloader, transform_params, model, current_dir)
 
-with open(os.path.join(current_dir, "loss.npy"), mode = 'wb') as f:
-    np.save(f, np.array(loss_list))
+
+with open(os.path.join(current_dir, "logger.npy"), mode = 'wb') as f:
+    np.save(f, np.array(logger['loss']))
+    np.save(f, np.array(logger['psnr']))
