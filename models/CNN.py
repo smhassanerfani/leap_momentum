2804130ec40c92a3767a06df493343afa11d25b7
smhassanerfani <serfani@email.sc.edu>
2023-07-17 18:37:07 +0000

    Added simple CNN model

---
+++ models/CNN.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.CNN = nn.Sequential(
+            nn.Conv2d(1, 8, 7, padding=(3, 3), padding_mode='reflect'), nn.PReLU(),
+            nn.Conv2d(8, 8, 7, padding=(3, 3), padding_mode='reflect'), nn.PReLU(),
+            nn.Conv2d(8, 8, 7, padding=(3, 3), padding_mode='reflect'), nn.PReLU(),
+            nn.Conv2d(8, 8, 7, padding=(3, 3), padding_mode='reflect'), nn.PReLU(),
+            nn.Conv2d(8, 1, 7, padding=(3, 3), padding_mode='reflect'), nn.Tanh()
+        )
+    def forward(self, x):
+        x = self.CNN(x)
+        return x
+        
