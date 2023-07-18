5ff7fd55ace670a926777f2fd9413bb7f5627f26
smhassanerfani <serfani@email.sc.edu>
2023-07-18 21:56:54 +0000

    Added keyword arguments for activation function and multiple head

--- models/CNN.py
+++ models/CNN.py
@@ -2,14 +2,15 @@ import torch
 import torch.nn as nn
 
 class CNN(nn.Module):
-    def __init__(self):
+    def __init__(self, second_component=False, head_activation=None): # nn.Tanh()
         super(CNN, self).__init__()
         self.CNN = nn.Sequential(
             nn.Conv2d(1, 8, 7, padding=(3, 3), padding_mode='reflect'), nn.PReLU(),
             nn.Conv2d(8, 8, 7, padding=(3, 3), padding_mode='reflect'), nn.PReLU(),
             nn.Conv2d(8, 8, 7, padding=(3, 3), padding_mode='reflect'), nn.PReLU(),
             nn.Conv2d(8, 8, 7, padding=(3, 3), padding_mode='reflect'), nn.PReLU(),
-            nn.Conv2d(8, 2, 7, padding=(3, 3), padding_mode='reflect'), nn.Tanh()
+            nn.Conv2d(8, 2, 7, padding=(3, 3), padding_mode='reflect') if second_component else nn.Conv2d(8, 1, 7, padding=(3, 3), padding_mode='reflect'),
+            head_activation if head_activation else nn.Identity()
         )
     def forward(self, x):
         x = self.CNN(x)
