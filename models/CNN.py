3853ad9f7344b37a6d85410be32ebad5997a93ce
smhassanerfani <serfani@email.sc.edu>
2023-07-18 12:08:29 +0000

    Tested for transfer learning

--- models/CNN.py
+++ models/CNN.py
@@ -9,7 +9,7 @@ class CNN(nn.Module):
             nn.Conv2d(8, 8, 7, padding=(3, 3), padding_mode='reflect'), nn.PReLU(),
             nn.Conv2d(8, 8, 7, padding=(3, 3), padding_mode='reflect'), nn.PReLU(),
             nn.Conv2d(8, 8, 7, padding=(3, 3), padding_mode='reflect'), nn.PReLU(),
-            nn.Conv2d(8, 1, 7, padding=(3, 3), padding_mode='reflect'), nn.Tanh()
+            nn.Conv2d(8, 2, 7, padding=(3, 3), padding_mode='reflect'), nn.Tanh()
         )
     def forward(self, x):
         x = self.CNN(x)
